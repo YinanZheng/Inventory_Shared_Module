573fcb711abae7ba4741823183bef0457194f089
Yinan Zheng <y-zheng@northwestern.edu>
2025-02-15 03:56:09 -0600

    Update utils.R

--- utils.R
+++ utils.R
@@ -2150,7 +2150,7 @@ render_request_board <- function(requests, output_id, output) {
             class = "note-card",
             style = sprintf("
               position: relative;
-              width: 320px;
+              width: 300px;
               background-color: %s;
               border: 2px solid %s;
               border-radius: 10px;
