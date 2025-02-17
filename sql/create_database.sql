ec00d5a494598f943d79306a7134a59e7493671c
Yinan Zheng <y-zheng@northwestern.edu>
2025-02-17 00:37:57 -0600

    Update create_database.sql

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -40,7 +40,7 @@ CREATE TABLE `unique_items` (
   `SKU` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `ProductCost` decimal(10,2) NOT NULL,
   `DomesticShippingCost` float NOT NULL DEFAULT '0',
-  `Status` enum('采购','国内入库','国内出库','国内售出','美国入库','美国发货','美国调货','退货','完成') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Status` enum('采购','国内入库','国内出库','国内售出','美国入库','美国发货','美国调货','交易完毕') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `Defect` enum('未知','无瑕','瑕疵','修复') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `DefectNotes` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `PurchaseTime` date DEFAULT NULL,
@@ -50,7 +50,6 @@ CREATE TABLE `unique_items` (
   `UsEntryTime` date DEFAULT NULL,
   `UsShippingTime` date DEFAULT NULL,
   `UsRelocationTime` date DEFAULT NULL,
-  `ReturnTime` date DEFAULT NULL,
   `IntlShippingMethod` enum('海运','空运') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `IntlTracking` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `IntlShippingCost` float NOT NULL DEFAULT '0',
@@ -65,7 +64,7 @@ CREATE TABLE `unique_items` (
   CONSTRAINT `fk_intl_tracking` FOREIGN KEY (`IntlTracking`) REFERENCES `intl_shipments` (`TrackingNumber`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `fk_orderid` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`OrderID`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `unique_items_ibfk_1` FOREIGN KEY (`SKU`) REFERENCES `inventory` (`SKU`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci |
 
 CREATE TABLE `orders` (
   `OrderID` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
