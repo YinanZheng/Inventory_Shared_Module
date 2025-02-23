b69f005045555fb1e054ea018d80d1ea76ef84de
Yinan Zheng <y-zheng@northwestern.edu>
2025-02-23 07:18:49 -0600

    Update typeModuleUI.R

--- typeModuleUI.R
+++ typeModuleUI.R
@@ -4,7 +4,7 @@ typeModuleUI <- function(id) {
     fluidRow(
       column(10, uiOutput(ns("major_type_ui"))),  # 大类下拉框
       column(2, 
-             div(style = "display: flex; justify-content: center; align-items: center; height: 100%;", 
+             div(style = "display: flex; justify-content: center; align-items: center; height: 100%; margin: 0;", 
                  actionButton(ns("add_major_type_btn"), label = NULL, icon = icon("plus"),
                               style = "font-size: 14px; width: 100%; height: 34px; padding: 0px; margin-top: 0px;")
              )
@@ -14,7 +14,7 @@ typeModuleUI <- function(id) {
     fluidRow(
       column(10, uiOutput(ns("minor_type_ui"))),  # 小类下拉框
       column(2, 
-             div(style = "display: flex; justify-content: center; align-items: center; height: 100%;", 
+             div(style = "display: flex; justify-content: center; align-items: center; height: 100%; margin: 0;", 
                  actionButton(ns("add_minor_type_btn"), label = NULL, icon = icon("plus"),
                               style = "font-size: 14px; width: 100%; height: 34px; padding: 0px; margin-top: 0px;")
              )
